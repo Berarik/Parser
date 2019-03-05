dec6bc1dbef66d653541391a86e89d71bd675a0b
user_ <user_@REDBORODAPC>
2019-03-05 15:46:28 +0700

    Добавьте файлы проекта.

---
+++ ParserHTML/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParserHTML.Startup))]
+namespace ParserHTML
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
